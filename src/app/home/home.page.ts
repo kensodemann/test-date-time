62a7aec85d6823b5501d2e3449fdcb103630de71
Ken Sodemann <ken@ionic.io>
2024-06-27 17:24:02 -0500

    add platforms

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,16 +12,16 @@ import { startOfDay, parseISO } from 'date-fns';
 export class HomePage {
   d1 = new Date('2024-06-27');
   d2 = new Date('2024-06-27T03:33:23Z');
-  d3 = new Date('2024-06-27T03:33:23-05:00');
+  d3 = new Date('2024-06-27T03:33:23-04:00');
   d4 = new Date('2024-06-27T03:33:23+04:00');
 
   df1 = parseISO('2024-06-27');
   df2 = parseISO('2024-06-27T03:33:23Z');
-  df3 = parseISO('2024-06-27T03:33:23-05:00');
+  df3 = parseISO('2024-06-27T03:33:23-04:00');
   df4 = parseISO('2024-06-27T03:33:23+04:00');
 
   dfn1 = startOfDay(parseISO('2024-06-27'));
   dfn2 = startOfDay(parseISO('2024-06-27T03:33:23Z'));
-  dfn3 = startOfDay(parseISO('2024-06-27T03:33:23-05:00'));
+  dfn3 = startOfDay(parseISO('2024-06-27T03:33:23-04:00'));
   dfn4 = startOfDay(parseISO('2024-06-27T03:33:23+04:00'));
 }
