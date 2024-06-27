11e3ba5df3d0936bf2d41fad1f46cd3fc61bffa7
Ken Sodemann <ken@ionic.io>
2024-06-27 17:04:37 -0500

    show dates

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { startOfDay, parseISO } from 'date-fns';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +10,18 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
-  constructor() {}
+  d1 = new Date('2024-06-27');
+  d2 = new Date('2024-06-27T03:33:23Z');
+  d3 = new Date('2024-06-27T03:33:23-05:00');
+  d4 = new Date('2024-06-27T03:33:23+04:00');
+
+  df1 = parseISO('2024-06-27');
+  df2 = parseISO('2024-06-27T03:33:23Z');
+  df3 = parseISO('2024-06-27T03:33:23-05:00');
+  df4 = parseISO('2024-06-27T03:33:23+04:00');
+
+  dfn1 = startOfDay(parseISO('2024-06-27'));
+  dfn2 = startOfDay(parseISO('2024-06-27T03:33:23Z'));
+  dfn3 = startOfDay(parseISO('2024-06-27T03:33:23-05:00'));
+  dfn4 = startOfDay(parseISO('2024-06-27T03:33:23+04:00'));
 }
